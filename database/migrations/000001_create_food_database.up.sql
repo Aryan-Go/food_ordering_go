8ade436f7ffc8e4a80d4ef4ed31c1f1f85f3cbef
Aryan Goyal <goyal.aryan@gmail.com>
2025-08-06 22:27:30 +0530

    [FEAT] add migrations for the database food and table user
    Signed-off-by: Aryan Goyal <goyal.aryan@gmail.com>

---
+++ database/migrations/000001_create_food_database.up.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE food;
+USE food;
---
+++ database/migrations/000002_create_user_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE user(
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(200) NOT NULL ,
+    username VARCHAR(300),
+    password VARCHAR(200) NOT NULL,
+    role ENUM('chef','customer','admin')
+);
+INSERT INTO user (email,username,password,role) VALUES("admin@gmail.com","admin","$2a$10$szHaFxbefg73iCmJmxm3J.wiQvhLKPtsUUSgTILi7lZraS8y1y72y","admin");
+INSERT INTO user (email,username,password,role) VALUES("chef@gmail.com","chef","$2a$10$CjFpMrh7fguKKVLqeB.a6uZ1dacrq.vmMTFH6MG8CG0WLh8PQc4IG","chef");
